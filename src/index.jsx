abe2641341b1ac3fb9f2100b5e089703c8f0bd99
Jesper Olsson <software@jesperolsson.se>
2022-09-12 07:44:00 +0200

    #3 List epics

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import api, { route } from "@forge/api";
-import ForgeUI, { DashboardGadget, DashboardGadgetEdit, render, Text, TextField, useProductContext } from "@forge/ui";
+import ApiEpics from "./epics"
+import ForgeUI, { DashboardGadget, DashboardGadgetEdit, render, Text, useProductContext, useState, Select, Option } from "@forge/ui";
 
 const View = () => {
   const { extensionContext: { gadgetConfiguration } } = useProductContext();
@@ -7,7 +7,7 @@ const View = () => {
   return (
     <DashboardGadget>
       <Text
-        content={`Hello ${gadgetConfiguration.name || "world"}.`}
+        content={`Hello ${gadgetConfiguration.epic || "world"}.`}
       />
     </DashboardGadget>
   );
@@ -17,13 +17,23 @@ const Edit = () => {
   const onSubmit = values => {
       return values
   };
-
   return (
     <DashboardGadgetEdit onSubmit={onSubmit}>
-      <TextField name="name" label="Say hello to:" />
+      {useState(async () => await epicsSelect())[0]}
     </DashboardGadgetEdit>
   );
 }
+
+const epicsSelect = async () => {
+  const response = await new ApiEpics().epics();
+  const data = await response.json();
+  const list = data.issues.map(
+    (issue) => <Option label={`${issue.key} ${issue.fields.summary}`} value={issue.key} />
+  )
+  return <Select label="Epic" name="epic">
+        {list}
+      </Select>;
+};
 
 export const runView = render(
   <View/>
